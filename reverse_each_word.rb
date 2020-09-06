1ab98c43cf0e45d88954de4c45da21c8ec7dbd55
Evelyn Hernandez <hernandeze791@gmail.com>
2020-09-06 23:58:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(sentence)
-  sentence = ["Hello there, how are you?"]
-  sentence.each do |sentence|
-    puts "#{sentence}".reverse
-
-
+  sentence.split.collect { |word| word.reverse}.join("")
 end
